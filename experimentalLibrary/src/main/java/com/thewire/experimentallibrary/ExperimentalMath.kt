1d8b6ebb4f47dd9117b5ac8f8b4ae2a4f83aab55
TheWire <TheWire@users.noreply.github.com>
2023-01-23 02:38:12 +0000

    added add function

--- experimentalLibrary/src/main/java/com/thewire/experimentallibrary/ExperimentalMath.kt
+++ experimentalLibrary/src/main/java/com/thewire/experimentallibrary/ExperimentalMath.kt
@@ -5,10 +5,15 @@ import kotlin.math.pow
 
 class ExperimentalMath {
     fun cube(num: Float): Float {
+        println(add(num, num))
         return num.pow(3)
     }
 
     fun cubeRoot(num: Float): Float {
         return cbrt(num.toDouble()).toFloat()
+    }
+
+    fun add(a: Float, b: Float): Float {
+        return a+b
     }
 }
